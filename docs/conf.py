f75e320c2cbca8924afe55e0fbb883a8436560ea
hacihabiboglu <43171900+hacihabiboglu@users.noreply.github.com>
2022-12-23 19:27:44 +0300

    Documentation

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import sys
 sys.path.insert(0, os.path.abspath("../src"))
 sys.path.insert(0, os.path.abspath("../.."))
 
-
+autodoc_mock_imports = ["_numpy"]
 
 project = 'sphstat'
 copyright = '2022, Huseyin Hacihabiboglu'
